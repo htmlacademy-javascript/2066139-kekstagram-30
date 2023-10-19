d1d155ae815ab0acf378e49ec23a9c7bed1973c7
GitRemka <roman141999@mail.ru>
2023-10-19 17:37:14 +0700

    Добавляет рефакторинг

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
-/* eslint-disable no-unused-vars */
-const NAMES = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон',
-  'Робин',
-  'Питер',
-  'Мэри',
-];
-const DESCRIPTION = [
+const GENERATED_POST_COUNT = 25;
+const COMMENTS_IN_POST_MAX_COUNT = 30;
+const AVATAR_COUNT = 6;
+const NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Виктор', 'Юлия', 'Робин', 'Питер', 'Мэри',];
+const DESCRIPTIONS = [
   'Давайте наберем 100 лайков под этим фото!',
   'Бесконечное переплетение линий, создающее ощущение движения в статике.',
   'Пространство, заполненное плавными формами и переходами цветов, ведущее к гармонии и балансу.',
@@ -29,8 +19,6 @@ const MESSAGES_COMMENT = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
-const GENERATED_POST_COUNT = 25;
-const COMMENTS_IN_POST_MAX_COUNT = 30;
 
 const getRandomInteger = (min, max) => {
   const lower = Math.ceil(Math.min(min, max));
@@ -47,8 +35,6 @@ const createRandomIdFromRange = (min, max) => {
     let currentValue = getRandomInteger(min, max);
 
     if (previousValues.length >= (max - min + 1)) {
-      // eslint-disable-next-line no-console
-      console.error(`Перебраны все числа из диапазона от ${min} до ${max}`);
       return null;
     }
 
@@ -62,27 +48,25 @@ const createRandomIdFromRange = (min, max) => {
   };
 };
 
+const generateUserId = createRandomIdFromRange(1, GENERATED_POST_COUNT);
 const generateCommentId = createRandomIdFromRange(1, GENERATED_POST_COUNT * COMMENTS_IN_POST_MAX_COUNT);
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-const generateUserId = createRandomIdFromRange(1, 25);
-const generatePhotoId = createRandomIdFromRange(1, 25);
 
-const createCommentsArray = () => ({
+const createComment = () => ({
   commentsId: generateCommentId(),
-  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message: MESSAGES_COMMENT[Math.round(Math.random() * (MESSAGES_COMMENT.length - 1))],
+  avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
+  message: getRandomArrayElement(MESSAGES_COMMENT),
   name: getRandomArrayElement(NAMES),
 });
 
-const createUserPhoto = () => ({
+const createUserPhoto = (index) => ({
   userId: generateUserId(),
-  urlPhoto: `photos/${generatePhotoId()}.jpg`,
-  description: getRandomArrayElement(DESCRIPTION),
+  urlPhoto: `photos/${index}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
   likes: getRandomInteger(15, 200),
-  comments: Array.from({length: getRandomInteger(0, 30)}, createCommentsArray),
+  comments: Array.from({length: getRandomInteger(0, COMMENTS_IN_POST_MAX_COUNT)}, createComment),
 });
 
-const generatedPosts = Array.from({length: GENERATED_POST_COUNT}, createUserPhoto);
+const generatedPosts = Array.from({length: GENERATED_POST_COUNT}, (_, postIndex) => createUserPhoto(++postIndex));
 // eslint-disable-next-line no-console
 console.table(generatedPosts);
-
